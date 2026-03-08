8a850ee13bb45895d1828cf9e67b18779b30940f
SASWAT SUBHRA NAYAK <70335635+saswat2239@users.noreply.github.com>
2026-03-08 08:34:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopBtn = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    if(currentAudio){
+      currentAudio.pause();
+    }
+
+    const sound = button.getAttribute("data-sound");
+    currentAudio = new Audio(`sounds/${sound}.mp3`);
+    currentAudio.play();
+
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  if(currentAudio){
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
